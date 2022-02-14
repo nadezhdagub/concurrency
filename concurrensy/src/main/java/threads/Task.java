ed1ddbfa81e6d903381fc901fbf2f8c6def09fc2
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-02-15 01:34:55 +0300

    Add files via upload

--- concurrensy/src/main/java/threads/Task.java
+++ concurrensy/src/main/java/threads/Task.java
@@ -2,16 +2,37 @@ package threads;
 
 class MyTask implements Runnable {
 
+    int i = 0;
+
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Thread " + i);
-            try {
+        try {
+            while (true) {
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+                System.out.println("i=" + (++i));
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+        } catch (InterruptedException e){
+            cleanup();
         }
+
+//        for (int i = 0; i < 100; i++) {
+//            if (Thread.interrupted()) {
+//                System.out.println("Thread is interrupted");
+//                break;
+//            }
+//            System.out.println("Thread " + i);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+    void cleanup() {
+        System.out.println("Cleaning up");
     }
 }
 
@@ -22,6 +43,9 @@ public class Task {
         Thread t = new MyThread(task);
        // t.setDaemon(true);
         t.start();
+        Thread.sleep(2000);
+        t.interrupt();
+       // t.stop();
        // Thread.currentThread().sleep(2000);
         System.out.println("Main thread acquired exist");
     }
