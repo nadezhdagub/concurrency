c740b58bb6da54f371bc69e620ff8cd8b6529e6c
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-02-28 01:25:58 +0300

    Add files via upload

--- concurrensy/src/main/java/ThreadPoolExecutorEx/Main.java
+++ concurrensy/src/main/java/ThreadPoolExecutorEx/Main.java
@@ -1,11 +1,30 @@
 package ThreadPoolExecutorEx;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        new ThreadPoolExecutor(2, 2, 0, TimeUnit.SECONDS, new SynchronousQueue<>());
+        //ThreadPoolExecutor es = new ThreadPoolExecutor(2, 2, 0, TimeUnit.SECONDS, new SynchronousQueue<>());
+        ThreadPoolExecutor es = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2));
+        es.submit(() -> {
+            System.out.println("Task 1");
+            Thread.sleep(5000);
+            return null;
+        });
+
+        es.submit(() -> {
+            System.out.println("Task 2");
+            Thread.sleep(5000);
+            return null;
+        });
+
+        es.submit(() -> {
+            System.out.println("Task 3");
+            Thread.sleep(5000);
+            return null;
+        });
     }
 }
