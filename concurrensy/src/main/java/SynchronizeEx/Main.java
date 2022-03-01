7b64d441c1685bece7c00bd78c6312848a5c06d6
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-03-01 03:07:31 +0300

    Add files via upload

---
+++ concurrensy/src/main/java/SynchronizeEx/Main.java
@@ -0,0 +1,64 @@
+package SynchronizeEx;
+
+import java.util.concurrent.Callable;
+
+interface Counter {
+    void increment();
+    int get();
+}
+
+class Task implements Callable<Void> {
+
+    private final String name;
+
+    Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        System.out.println("Task " + name + " started in " + Thread.currentThread().getName());
+        Thread.sleep(3000);
+        System.out.println("Task " + name + " ended");
+        return null;
+    }
+}
+
+class CounterImpl implements Counter {
+    int count;
+
+    @Override
+    public void increment() {
+        count++;
+    }
+
+    @Override
+    public int get() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Counter counter = new CounterImpl();
+        countPrimers(1, 10000, counter);
+        System.out.println("Prime nummbers count: " + counter.get());
+    }
+
+    private static void countPrimers(int from, int to, Counter counter) {
+        for (int i = from; i<=to; ++i) {
+            if(isPrime(i)) {
+                counter.increment();
+            }
+        }
+    }
+
+    private static boolean isPrime(int val) {
+        if(val == 1) return false;
+        if (val == 2) return true;
+        for (int i = 3; i <= val-1; ++i){
+            if(val%i == 0) return false;
+        }
+        return true;
+    }
+}
--- concurrensy/src/main/java/ThreadPoolExecutorEx/Main.java
+++ concurrensy/src/main/java/ThreadPoolExecutorEx/Main.java
@@ -1,14 +1,21 @@
 package ThreadPoolExecutorEx;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
         //ThreadPoolExecutor es = new ThreadPoolExecutor(2, 2, 0, TimeUnit.SECONDS, new SynchronousQueue<>());
-        ThreadPoolExecutor es = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2));
+        ThreadPoolExecutor es = new ThreadPoolExecutor(1,
+                1,
+                0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(2),
+                new RejectedExecutionHandler() {
+                    @Override
+                    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                        executor.setMaximumPoolSize(executor.getLargestPoolSize() + 1);
+                        executor.submit(r);
+                    }
+                });
         es.submit(() -> {
             System.out.println("Task 1");
             Thread.sleep(5000);
