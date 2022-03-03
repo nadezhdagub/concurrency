95af7169e3cc066adf9063990d668b846989ce1a
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-03-04 01:44:27 +0300

    Add files via upload

--- concurrensy/src/main/java/SynchronizeEx/Main2.java
+++ concurrensy/src/main/java/SynchronizeEx/Main2.java
@@ -3,17 +3,44 @@ package SynchronizeEx;
 import java.util.concurrent.*;
 
 interface FactorialProducer {
-    long calcFactorial(int val);
+    long calcFactorial(int val) throws InterruptedException;
 }
 
 class FactorialProducerImpl implements FactorialProducer {
 
+    static class LastState {
+        final int arg;
+        final long res;
+
+        LastState(int arg, long res) {
+            this.arg = arg;
+            this.res = res;
+        }
+
+        long getCachedValue(int val) {
+            if (val == arg) {
+                return res;
+            } else return 0;
+        }
+    }
+
+    int lastArg;
+    long lastRes;
+
     @Override
-    public long calcFactorial(int val) {
+    public synchronized long calcFactorial(int val) throws InterruptedException {
+        System.out.println("Entering calcFactorial " + Thread.currentThread().getName());
+        if(lastArg == val) {
+            return lastRes;
+        }
+        lastArg = val;
+
+        Thread.sleep(3000);
         long res = 1;
-        for (int i = 1; i < val; ++i) {
+        for (int i = 1; i <= val; ++i) {
             res += i;
         }
+        lastRes = res;
         return res;
     }
 }
